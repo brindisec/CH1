75863171fd3d0f357447836f2852a1851f2d5343
brindisec <56162715+brindisec@users.noreply.github.com>
2020-01-04 22:16:19 -0500

    Add files via upload

---
+++ Odd2.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+import random
+import time
+odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
+         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+         41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
+
+for i in range(5):
+    right_this_minute = datetime.today() .minute
+    if right_this_minute in odds:
+        print("This minute seems a little odd.")
+    else:
+        print("Not an odd minute.")
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
+beersong.py
+word = "bottles"
+for beer_num in range(99, 0, -1):
+    print(beer_num, word, "of beer on the wall.")
+    print(beer_num, word, "of beer.")
+    print("Take one down.")
+    print("Pass it around.")
+    if beer_num == 1:
+        print("No more bottles of beer on the wall.")
+    else:
+        new_num = beer_num - 1
+        if new_num == 1:
+            word = "bottle"
+        print(new_num, word, "of beer on the wall.")
+    print()
